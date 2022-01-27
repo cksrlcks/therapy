3ffefb93337f810e93d53dcb79d443df083973f2
chanki <chanki.kim89@gmail.com>
2022-01-27 14:44:47 +0900

    styled component, favicon 설정

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,115 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(
+            (App) => (props) => sheet.collectStyles(<App {...props} />)
+        );
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
     }
 
     render() {
         return (
-            <Html lang="ko">
+            <html lang="ko">
                 <Head>
-                    <title>감각운동심리치료사 조성호</title>
                     <meta
-                        name="description"
-                        content="감각운동심리치료사 조성호"
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
                     />
-                    <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="57x57"
+                        href="/img/favicons/apple-icon-57x57.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="60x60"
+                        href="/img/favicons/apple-icon-60x60.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="72x72"
+                        href="/img/favicons/apple-icon-72x72.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="76x76"
+                        href="/img/favicons/apple-icon-76x76.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="114x114"
+                        href="/img/favicons/apple-icon-114x114.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="120x120"
+                        href="/img/favicons/apple-icon-120x120.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="144x144"
+                        href="/img/favicons/apple-icon-144x144.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="152x152"
+                        href="/img/favicons/apple-icon-152x152.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/img/favicons/apple-icon-180x180.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="/img/favicons/android-icon-192x192.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/img/favicons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="96x96"
+                        href="/img/favicons/favicon-96x96.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/img/favicons/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/img/favicons/manifest.json" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta
+                        name="msapplication-TileImage"
+                        content="/img/favicons/ms-icon-144x144.png"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+                    {/* seo */}
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="홈페이지 이름" />
+                    <meta property="og:site_name" content="홈페이지 이름" />
+                    <meta property="og:url" content="홈페이지 주소" />
+                    <meta property="og:image" content="썸네일 이미지 경로" />
+                    <meta property="og:image:width" content="800" />
+                    <meta property="og:image:height" content="400" />
+                    {this.props.styleTags}
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
                 </body>
-            </Html>
+            </html>
         );
     }
 }
-
-export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from "next/head";
-
 export default function Home() {
     return (
         <div className="wrap">
-            <Head>
-                <title>감각운동심리치료사 조성호 | 안녕하세요</title>
-                <meta name="description" content="감각운동심리치료사 조성호" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div>메인내용</div>
+            <div>
+                <a href="https://line.me/R/ti/p/waterweb1">라인</a>
+            </div>
         </div>
     );
 }
