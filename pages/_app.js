752f59b0d09fc74e24e02baeea15674f01c0f4fb
chanki <chanki.kim89@gmail.com>
2022-01-27 15:34:33 +0900

    ie리다이렉트설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import "../styles/reset.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+            window.location = "microsoft-edge:" + window.location;
+            setTimeout(function () {
+                window.location = "/browserSupport.html";
+            }, 1000);
+        }
+    }, []);
     return <Component {...pageProps} />;
 }
 
