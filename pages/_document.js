daac643dd0b1521289a9f178d21ef6c0ac917da1
cksrlcks <44834561+cksrlcks@users.noreply.github.com>
2022-01-30 15:59:27 +0900

    styledcomponenet 수정

--- pages/_document.js
+++ pages/_document.js
@@ -110,6 +110,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
