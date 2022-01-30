463f581427fdb23ad6e9e80305ecd71822a264c1
cksrlcks <44834561+cksrlcks@users.noreply.github.com>
2022-01-30 15:52:00 +0900

    구글폰트 수정

--- pages/_document.js
+++ pages/_document.js
@@ -1,124 +1,121 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-    static getInitialProps({ renderPage }) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage(
-            (App) => (props) => sheet.collectStyles(<App {...props} />)
-        );
-        const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
-    }
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
 
-    render() {
-        return (
-            <html lang="ko">
-                <Head>
-                    <meta
-                        name="viewport"
-                        content="initial-scale=1.0, width=device-width"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="57x57"
-                        href="/img/favicons/apple-icon-57x57.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="60x60"
-                        href="/img/favicons/apple-icon-60x60.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="72x72"
-                        href="/img/favicons/apple-icon-72x72.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="76x76"
-                        href="/img/favicons/apple-icon-76x76.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="114x114"
-                        href="/img/favicons/apple-icon-114x114.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="120x120"
-                        href="/img/favicons/apple-icon-120x120.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="144x144"
-                        href="/img/favicons/apple-icon-144x144.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="152x152"
-                        href="/img/favicons/apple-icon-152x152.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/img/favicons/apple-icon-180x180.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="192x192"
-                        href="/img/favicons/android-icon-192x192.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/img/favicons/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="96x96"
-                        href="/img/favicons/favicon-96x96.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/img/favicons/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/img/favicons/manifest.json" />
-                    <meta name="msapplication-TileColor" content="#ffffff" />
-                    <meta
-                        name="msapplication-TileImage"
-                        content="/img/favicons/ms-icon-144x144.png"
-                    />
-                    <meta name="theme-color" content="#ffffff" />
-                    {/* seo */}
-                    <meta property="og:type" content="website" />
-                    <meta
-                        property="og:title"
-                        content="감각운동심리치료사 조성호"
-                    />
-                    <meta
-                        property="og:site_name"
-                        content="감각운동심리치료사 조성호"
-                    />
-                    <meta
-                        property="og:url"
-                        content="https://spkorea.netlify.app"
-                    />
-                    <meta property="og:image" content="/img/og-img.png" />
-                    <meta property="og:image:width" content="800" />
-                    <meta property="og:image:height" content="400" />
-                    {this.props.styleTags}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </html>
-        );
-    }
+  render() {
+    return (
+      <html lang="ko">
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/img/favicons/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/img/favicons/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/img/favicons/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/img/favicons/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/img/favicons/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/img/favicons/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/img/favicons/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/img/favicons/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/favicons/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/img/favicons/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/img/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/img/favicons/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/img/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/img/favicons/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-TileImage"
+            content="/img/favicons/ms-icon-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
+          {/* seo */}
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="감각운동심리치료사 조성호" />
+          <meta property="og:site_name" content="감각운동심리치료사 조성호" />
+          <meta property="og:url" content="https://spkorea.netlify.app" />
+          <meta property="og:image" content="/img/og-img.png" />
+          <meta property="og:image:width" content="800" />
+          <meta property="og:image:height" content="400" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
 }
